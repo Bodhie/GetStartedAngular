08154cbb9963a251fc8142b683092dee9c88e522
Bodhie <stefanbode1995@hotmail.com>
2017-12-09 13:03:02 +0100

    Until step 7 routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  getHeroes(): Observable<Hero[]> {
-    return of(HEROES);
-  }
+    constructor(private messageService: MessageService) { }
 
-  constructor() { }
+    getHeroes(): Observable<Hero[]> {
+        this.messageService.add('HeroService: fetched heroes');
+        return of(HEROES);
+    }
 
+    getHero(id: number): Observable<Hero> {
+        this.messageService.add(`HeroService: fetched hero id=${id}`);
+        return of(HEROES.find(hero => hero.id === id));
+    }
 }
